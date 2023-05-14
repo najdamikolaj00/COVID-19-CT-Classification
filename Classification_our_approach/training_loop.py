813d20619dec58fd845815d9972d1636a4cffbea
Mikolaj Najda <najda.mikolaj00@gmail.com>
2023-05-14 10:31:17 +0200

    additional comments for training_loop to clarify the process

--- Classification_our_approach/training_loop.py
+++ Classification_our_approach/training_loop.py
@@ -19,15 +19,16 @@ def training_loop(model, optimizer, loss_function, k_folds, train_loaders, val_l
                 loss = loss_function(outputs, labels)
                 
                 # Backward pass and optimization
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
+                optimizer.zero_grad() #zeroes out the gradients of all the model parameters.
+                loss.backward() #computes the gradients of the model's parameters with respect to the loss
+                optimizer.step() #updates the model's parameters using the computed gradients and the chosen optimization algorithm
 
             # Validation
             model.eval()
             val_loss = 0.0
             correct = 0
             total = 0
+            #torch.no_grad ensures that no gradients are computed during this process, as we don't need them for evaluation.
             with torch.no_grad():
                 for batch in val_loader:
                     inputs = batch['img']
